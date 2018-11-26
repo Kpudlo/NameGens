eaaf3b79f88b3183802a9df6abc3eea20c42ce14
Kevin Pudlo <pudlok@gmail.com>
2018-11-26 16:21:36 -0500

    Update wolvan.js

--- names/wolvan.js
+++ names/wolvan.js
@@ -1,1 +1,16 @@
-
+gen_data['wolvan'] = [
+    'A {gender} wolvan named {first} {last}. <br />'
+  ];
+  gen_data['gender'] = [
+    'male', 'female'
+  ];
+  gen_data['first'] = [
+    'Pendros',
+    'Olengar',
+    'Delros'
+  ];
+  gen_data['last'] = [
+    'Snaggletooth',
+    'Flaretongue',
+    'Hammerstein'
+  ];
